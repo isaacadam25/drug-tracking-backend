442f7c43d40792d214225e9a114a93fd452504f5
pyskywalker <79524205+pyskywalker@users.noreply.github.com>
2021-07-15 12:14:13 +0300

    makolo_benja_align

--- Benja/urls.py
+++ Benja/urls.py
@@ -5,6 +5,8 @@ from .views import *
 urlpatterns = [
     path('userprofile/',LoggedUserProfile.as_view(), name='singleuserprofile'),
     path('userslist/',UserProfileView.as_view(),name="userprofile"),
+    path('userlist/doctors/',GetAvailableDoctors.as_view(),name="get-doctors"),
+
     path('pharmacy/medicine',MedicineAPI.as_view(),name="medicine"),
     path('pharmacy/batches',BatchAPI.as_view(),name="batch"),
     path('pharmacy/batches/create',BatchAddAPI.as_view(),name="create-batch"),
@@ -16,10 +18,10 @@ urlpatterns = [
     #path('Appointment',AppointmentAPI.as_view(),name="appointment"),
     path('sales/transactions',TransactionAPI.as_view(),name="transactions"),
     path('hospital/labtests',LabtestAPI.as_view(),name="labtests"),
-    path('hospital/patients/',PatientAPI.as_view(),name="patients"),
     path('hospital/diagnoses',DiagnosesAPI.as_view(),name="patient-diagnoses"),
     path('hospital/diagnoses/<int:id>',SingleDiagnosesAPI.as_view(),name="single-diagnoses"),
-
+    
+    path('hospital/patients/',PatientAPI.as_view(),name="patients"),
     path('hospital/patient-type',PatientTypeAPI.as_view(),name="patient-type"),
     path('hospital/appointments/',AppointmentAPI.as_view(),name="appointments"),
     path('hospital/appointments/pending',PendingAppointmentAPI.as_view(),name="pending-appointment"),
@@ -28,7 +30,8 @@ urlpatterns = [
     path('hospital/appointment/<int:id>',SingleAppointmentAPI.as_view(),name="single-appointment"),
     path('hospital/prescriptions',Prescriptions.as_view(),name="prescriptions"),
     path('hospital/prescriptions/<int:id>',SinglePrescriptionAPI.as_view(),name="single-prescriptions"),
-    
+     path('hospital/prescriptions/pending',PrescriptionsPending.as_view(),name="prescriptions-pending"),
+    path('hospital/prescriptions/create',CreatePrescription.as_view(),name="create-prescriptions"),
     path('hospital/prescriptions/accept/<int:id>',AcceptPrescriptionAPI.as_view(),name="accept-prescriptions"),
     path('hospital/appointment/<int:id>/prescriptions',GetAppointmentPrescriptions.as_view(),name="appointment-prescriptions"),
 ]
